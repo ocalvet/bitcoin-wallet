1a998fe3c0c35897119dcb70a92b395b9ac2a09c
Ovidio R. Calvet <calveto@gmail.com>
2018-11-08 04:11:28 -0800

    Create frontend placeholder

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Bitcoin Wallet - Testing
+      </div>
+    );
+  }
+}
+
+export default App;
