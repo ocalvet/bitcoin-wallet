ee642864a9020083598459901e33bfeb5095bf13
Ovidio R. Calvet <calveto@gmail.com>
2018-11-09 14:20:11 -0800

    Add padding to whole application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core';
 import WalletList from './Wallet/WalletList';
 
 const wallets = [{ 
@@ -9,10 +10,17 @@ const wallets = [{
   description: 'A wallet to keep coins but not to be use externally'
 }];
 
+const styles = theme => ({
+  root: {
+    padding: theme.spacing.unit * 2
+  }
+});
+
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
+      <div className={classes.root}>
         <h1>Bitcoin Wallet - Testing</h1>
         <WalletList wallets={wallets} />
       </div>
@@ -20,4 +28,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
