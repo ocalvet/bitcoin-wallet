261c9490a8b03b48a49fc1a71acf78a0523198df
Ovidio R. Calvet <calveto@gmail.com>
2018-11-14 11:07:19 -0500

    Switch to use a card insted of paper

--- frontend/src/Wallet/Wallet.js
+++ frontend/src/Wallet/Wallet.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
@@ -12,24 +15,40 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 2,
     marginBottom: theme.spacing.unit * 2,
   },
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
 });
 
 function Wallet({ classes, wallet }) {
-
   return (
-    <div>
-      <Paper className={classes.root} elevation={1}>
-        <Typography variant="h5" component="h3">
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
           {wallet.title}
         </Typography>
         <Typography component="p">
           {wallet.description}
         </Typography>
         <Typography component="p">
-          Key: {wallet.key}
+          Private Key: {wallet.key}
         </Typography>
-      </Paper>
-    </div>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add Transaction</Button>
+      </CardActions>
+    </Card>
   );
 }
 
